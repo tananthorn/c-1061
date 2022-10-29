32c9c432174703027b2e0c9f65fe60f64cab12f2
tananthorn <89711608+tananthorn@users.noreply.github.com>
2022-10-29 16:43:29 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function startClassification()
+{
+navigator.mediaDevices.getUserMedia({audio: true});
+classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/B0QSpl4J1/model.json', modelReady);
+}
+function modelReady(){
+classifier.classify(gotResults);
+}
